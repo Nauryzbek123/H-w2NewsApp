8320be3df8118d19f65d080fe093ea7e32a291f9
DiasNauryzbek <200103153@stu.sdu.edu.kz>
2024-03-16 08:37:38 +0600

    added new functions for data

--- newsapi/src/main/java/dev/androidbroadcast/newsapi/NewsApi.kt
+++ newsapi/src/main/java/dev/androidbroadcast/newsapi/NewsApi.kt
@@ -10,6 +10,10 @@ import dev.androidbroadcast.newsapi.models.Language
 import dev.androidbroadcast.newsapi.models.Response
 import dev.androidbroadcast.newsapi.models.SortBy
 import kotlinx.serialization.Serializable
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.create
+import retrofit2.http.Url
 
 interface NewsApi {
 
@@ -18,11 +22,32 @@ interface NewsApi {
         @Query("q") query: String? = null,
         @Query("from") from: Date? = null,
         @Query("to") to: Date? = null,
-        @Query("to") languages: List<Language>? = null,
+        @Query("languages") languages: List<Language>? = null,
         @Query("sortBy") sortBy: SortBy? = null,
         @Query("pagesSize") @IntRange(from = 0, to = 180) pageSize: Int = 180,
         @Query("page") @IntRange(from = 1) page: Int = 1
         ): Response<Article>
 }
+
+    fun NewsApi(
+        baseUrl: String,
+        okHttpClient: OkHttpClient? = null
+    ): NewsApi{
+        val retrofit = retrofit(baseUrl,okHttpClient)
+
+        return  retrofit.create(NewsApi::class.java)
+
+    }
+
+    private fun retrofit(
+        baseUrl: String,
+        okHttpClient: OkHttpClient?
+    ): Retrofit{
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .run { if(okHttpClient != null) client(okHttpClient) else this }
+            .build()
+        return retrofit
+    }
 
 
