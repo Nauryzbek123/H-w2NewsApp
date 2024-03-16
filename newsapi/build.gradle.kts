3af875c6953ed1e3b8818fbde76cb759ee9bd29f
DiasNauryzbek <200103153@stu.sdu.edu.kz>
2024-03-16 07:28:28 +0600

    library importing part

---
+++ newsapi/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("java-library")
+    alias(libs.plugins.jetbrainsKotlinJvm)
+    alias(libs.plugins.kotlinSerialization)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+dependencies{
+    implementation(libs.retrofit)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.serialization.json)
+
+
+}
