6b7ce73ca446bb35b63873285a393dc6dcb11ceb
DiasNauryzbek <200103153@stu.sdu.edu.kz>
2024-03-23 15:46:11 +0600

    new modules

--- newsapi/src/main/java/dev/androidbroadcast/newsapi/NewsApi.kt
+++ newsapi/src/main/java/dev/androidbroadcast/newsapi/NewsApi.kt
@@ -1,15 +1,13 @@
 package dev.androidbroadcast.newsapi
 
-import kotlinx.serialization.SerialName
 import retrofit2.http.GET
 import retrofit2.http.Query
 import java.util.Date
 import androidx.annotation.IntRange
-import dev.androidbroadcast.newsapi.models.Article
+import dev.androidbroadcast.newsapi.models.ArticleDTO
 import dev.androidbroadcast.newsapi.models.Language
-import dev.androidbroadcast.newsapi.models.Response
+import dev.androidbroadcast.newsapi.models.ResponseDTO
 import dev.androidbroadcast.newsapi.models.SortBy
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.skydoves.retrofit.adapters.result.ResultCallAdapterFactory
@@ -19,7 +17,6 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.create
 import retrofit2.http.Header
-import retrofit2.http.Url
 
 interface NewsApi {
 
@@ -33,7 +30,7 @@ interface NewsApi {
         @Query("sortBy") sortBy: SortBy? = null,
         @Query("pagesSize") @IntRange(from = 0, to = 180) pageSize: Int = 180,
         @Query("page") @IntRange(from = 1) page: Int = 1
-        ): Result<Response<Article>>
+        ): Result<ResponseDTO<ArticleDTO>>
 }
 
     fun NewsApi(
