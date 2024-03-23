f887b03aae64b2c8868bb8e1ef7ddca60eec3016
DiasNauryzbek <200103153@stu.sdu.edu.kz>
2024-03-23 16:56:58 +0600

    created new database

--- database/src/main/java/dev/androidbroadcast/news/database/NewsDatabase.kt
+++ database/src/main/java/dev/androidbroadcast/news/database/NewsDatabase.kt
@@ -1,4 +1,26 @@
 package dev.androidbroadcast.news.database
 
-class NewsDatabase {
+import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.room.Database
+import androidx.room.DatabaseConfiguration
+import androidx.room.InvalidationTracker
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteOpenHelper
+import dev.androidbroadcast.news.database.dao.ArticleDao
+import dev.androidbroadcast.news.database.models.ArticleDBO
+
+@Database(entities = [ArticleDBO::class], version = 1)
+abstract class NewsDatabase : RoomDatabase() {
+  abstract fun articleDao(): ArticleDao
+
+}
+fun NewsDatabase(applicationContext: Context): NewsDatabase {
+    return Room.databaseBuilder(
+        checkNotNull(applicationContext.applicationContext),
+        NewsDatabase::class.java,
+        "news"
+    ).build()
 }
