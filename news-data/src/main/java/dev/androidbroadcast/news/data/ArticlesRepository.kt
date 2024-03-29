522b7e6c60e9857ab9d2141b7639f327fa195158
DiasNauryzbek <200103153@stu.sdu.edu.kz>
2024-03-30 01:11:12 +0500

    flow,dbo

--- news-data/src/main/java/dev/androidbroadcast/news/data/ArticlesRepository.kt
+++ news-data/src/main/java/dev/androidbroadcast/news/data/ArticlesRepository.kt
@@ -3,19 +3,43 @@ package dev.androidbroadcast.news.data
 import dev.androidbroadcast.news.data.model.Article
 import dev.androidbroadcast.news.database.NewsDatabase
 import dev.androidbroadcast.newsapi.NewsApi
+import dev.androidbroadcast.newsapi.models.ArticleDTO
+import dev.androidbroadcast.newsapi.models.ResponseDTO
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import java.io.IOException
 
 class ArticlesRepository(
     private val database: NewsDatabase,
     private val api: NewsApi,
 ) {
-     fun getAll(): RequestResult< Flow<List<Article>>>{
-        return RequestResult.InProgress(
-            database.articlesDao
-                .getAll()
-                .map { articles -> articles.map { it.toArticle() } }
-        )
+     fun getAll(): Flow<RequestResult<List<Article>>>{
+         val cachedAllArticles: Flow<List<Article>> = database.articlesDao
+             .getAll()
+             .map { articles -> articles.map { it.toArticle() } }
+
+         var remoteArticles = flow {
+             emit(api.everything())
+         }.map {result ->
+             if (result.isSuccess){
+                 val response: ResponseDTO<ArticleDTO> = result.getOrThrow()
+                 response.articles
+             }else{
+                 throw result.exceptionOrNull() ?: IOException()
+             }
+         }.map { articlesDtos ->
+             articlesDtos.map { articleDTO -> articleDTO.toArticleDbo() }
+         }.onEach { articlesDbos ->
+             database.articlesDao.insert(articlesDbos)
+         }
+
+        cachedAllArticles.map {
+
+        }
+         return cachedAllArticles.combine(remoteArticles)
     }
 
     suspend fun search(query:String): Flow<Article>{
@@ -23,6 +47,9 @@ class ArticlesRepository(
         TODO("Not implemented")
     }
 }
+
+
+
 sealed class RequestResult<E>(protected val data:E?) {
     class  InProgress<E>( data:E?): RequestResult<E>(data)
     class  Success<E>( data:E?): RequestResult<E>(data)
